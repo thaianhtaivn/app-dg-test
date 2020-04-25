2ac6bc9ff5afe7eed42d4a9a4b7c1d42775cb7eb
thaianhtaivn <thaianhtaivn>
2020-04-25 08:26:29 +0700

    Updates

---
+++ precache-manifest.1aadba77ff815705496636f2fc0870e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10d8459eed5abdee69528d82a6c68b88",
+    "url": "/app-dg-test/index.html"
+  },
+  {
+    "revision": "1e17d95b07196e9794b7",
+    "url": "/app-dg-test/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6c83d14abaec85ce6253",
+    "url": "/app-dg-test/static/css/main.1edc3a82.chunk.css"
+  },
+  {
+    "revision": "1e17d95b07196e9794b7",
+    "url": "/app-dg-test/static/js/2.0aec830b.chunk.js"
+  },
+  {
+    "revision": "c923b4d45708e3bc7e6704d27f16d3df",
+    "url": "/app-dg-test/static/js/2.0aec830b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c83d14abaec85ce6253",
+    "url": "/app-dg-test/static/js/main.a0a9ed8a.chunk.js"
+  },
+  {
+    "revision": "a03501315b9375a66cc3",
+    "url": "/app-dg-test/static/js/runtime-main.66b7bd0e.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/app-dg-test/static/media/logo.ee7cd8ed.svg"
+  }
+]);
